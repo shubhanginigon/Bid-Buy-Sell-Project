174d5222ed42c4283846578e5bab827d37b17cf0
Shubhi <shubhanginigontia@gmail.com>
2021-02-10 22:01:29 +0700

    Main Project folder created

---
+++ Main_Project/src/main/java/com/SAD/Main_Project/MainProjectApplication.java
@@ -0,0 +1,13 @@
+package com.SAD.Main_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainProjectApplication.class, args);
+	}
+
+}
